c888e21376be142049613730c9c0f905fc465d66
Andrea <36007844+andreasacco26@users.noreply.github.com>
2024-01-02 12:02:23 +0100

    Minor code adjustment

--- Assets/Scripts/Juice/BallHit.cs
+++ Assets/Scripts/Juice/BallHit.cs
@@ -2,8 +2,6 @@ using DG.Tweening;
 using UnityEngine;
 
 // TODO list:
-// Scale ball on hit
-// Change color on hit
 // Scale paddle based on mouse offset
 // Cutify: paddle face
 // Cutify: paddle face animation
@@ -18,7 +16,6 @@ public class BallHit : MonoBehaviour
     [SerializeField] float tweenDuration;
     private float brickSoundTimer = 0.0f;
     private int dings;
-
 
     private bool wallSoundActive = false;
     private bool paddleSoundActive = false;
@@ -42,35 +39,25 @@ public class BallHit : MonoBehaviour
         AudioManager.Instance.PlaySfx(brickSfxName);
     }
 
+    private void BallTween()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        Color originalColor = sr.color;
+        Sequence mySequence = DOTween.Sequence();
+        float tweenDuration = 0.015f;
+        mySequence.Append(transform.DOScale(0.5f, tweenDuration));
+        mySequence.Append(sr.DOColor(Color.white, tweenDuration));
+        mySequence.Append(transform.DOScale(0.3f, tweenDuration));
+        mySequence.Append(sr.DOColor(originalColor, tweenDuration));
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (wallSoundActive && other.gameObject.CompareTag("Wall"))
-        {
-            AudioManager.Instance.PlaySfx("ball-wall");
-        }
-        if (brickSoundActive && other.gameObject.CompareTag("Brick"))
-        {
-            PlayBrickSounds();
-        }
-        if (paddleSoundActive && other.gameObject.CompareTag("Paddle"))
-        {
-            AudioManager.Instance.PlaySfx("paddle");
-        }
-        if (cameraShakeActive)
-        {
-            CameraManager.Instance.ShakeCamera(amplitude, shakeTime);
-        }
-        if (ballTweenActive)
-        {
-            SpriteRenderer sr = GetComponent<SpriteRenderer>();
-            Color originalColor = sr.color;
-            Sequence mySequence = DOTween.Sequence();
-            float tweenDuration = 0.015f;
-            mySequence.Append(transform.DOScale(0.5f, tweenDuration));
-            mySequence.Append(sr.DOColor(Color.white, tweenDuration));
-            mySequence.Append(transform.DOScale(0.3f, tweenDuration));
-            mySequence.Append(sr.DOColor(originalColor, tweenDuration));
-        }
+        if (wallSoundActive && other.gameObject.CompareTag("Wall")) AudioManager.Instance.PlaySfx("ball-wall");
+        if (brickSoundActive && other.gameObject.CompareTag("Brick")) PlayBrickSounds();
+        if (paddleSoundActive && other.gameObject.CompareTag("Paddle")) AudioManager.Instance.PlaySfx("paddle");
+        if (cameraShakeActive) CameraManager.Instance.ShakeCamera(amplitude, shakeTime);
+        if (ballTweenActive) BallTween();
     }
 
     void Update()
